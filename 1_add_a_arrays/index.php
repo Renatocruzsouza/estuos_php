519b4a1980c399647203a15b3d54dbb77c1df29e
Renato Cruz Souza <renato.souza.flooxmongagua@gmail.com>
2025-06-25 01:40:06 -0300

    feat: add values to array

---
+++ 1_add_a_arrays/index.php
@@ -0,0 +1,24 @@
+<?php
+
+$arr = [];
+
+// Adicionano valores!!
+
+print_r($arr);
+echo "<br>";
+
+$arr[0] = 10;
+
+print_r($arr);
+echo "<br>";
+
+$arr[1] = 15;
+
+print_r($arr);
+echo "<br>";
+
+$arr[5] = 25;
+
+print_r($arr);
+echo "<br>";
+
