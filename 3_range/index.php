5106a3fa5439e00891fccd1ed855d1edfcd561e5
Renato Cruz Souza <renato.souza.flooxmongagua@gmail.com>
2025-06-25 02:12:30 -0300

    feat: sum and conditionally display array values

--- 3_range/index.php
+++ 3_range/index.php
@@ -10,9 +10,27 @@ $arr2 = range(5,20);
 print_r($arr2);
 echo "<br>";
 
-$arr3 = range(1,1000, 100);
+$arr3 = range(0,1000, 100);
 
 print_r($arr3);
+
+// exercicio 
+
+$arr4 = range(10, 45);
+
+for($i = 0; $i < count($arr4); $i++ ) {
+
+    $soma = $arr4[$i]+ 6;
+
+
+    if ($soma > 30) {
+
+        echo "O número $soma é muito alto <br>";
+
+    } else {
+        echo "$soma <br>";
+    }
+}
 
 
 
