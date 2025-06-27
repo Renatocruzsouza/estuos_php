e41508a213ba21a13fb3cce7600afa8dcfc58c43
Renato Cruz Souza <renato.souza.flooxmongagua@gmail.com>
2025-06-27 16:37:00 -0300

    feat: improve nested array loop and output formatting

--- 5_array_multidimensional/index.php
+++ 5_array_multidimensional/index.php
@@ -14,4 +14,27 @@ echo "<br>";
 print_r($arr[1][2]); // Acessando seguno array, e último elemento
 echo "<br>";
 
-print_r(count($arr));
+print_r(count($arr)); 
+echo "<br>";
+
+$arr2 = [
+    [1, 2, 3, 4],
+    [2, 4, 6, 8],
+    [3, 6, 9, 12]
+];
+// loop no array externo
+for($i =0; $i < count($arr2); $i++) {
+
+    // Imprimindo array
+    echo"Imprimindo array externo:" . ($i + 1);
+    echo "<br>";
+
+    // Imprimindo o array interno
+
+    for($j = 0; $j < count($arr2[$i]); $j++) {
+
+        echo $arr2[$i][$j];
+        echo "<br>";
+
+    }
+}
