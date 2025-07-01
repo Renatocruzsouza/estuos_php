edae7ff56344bc7a01f8d332af88a909f78c3b34
Renato Cruz Souza <renato.souza.flooxmongagua@gmail.com>
2025-07-01 20:21:32 -0300

    feat: add example using array_splice to remove elements from an array

---
+++ 11_array_splice/index.php
@@ -0,0 +1,29 @@
+<?php
+
+// resgatar elementos de array 
+// remover elementos
+
+$arr = [1, 2, 3, 4, 5, 6];
+
+$removidos = array_splice($arr, 1, 2);
+
+print_r($arr);
+echo "<br>";
+echo "<br>";
+
+print_r($removidos);
+echo "<br>";
+echo "<br>";
+
+
+$arr2 = [1, 2, 3, 4, 5, 6];
+
+$removidos2 = array_splice($arr2, 3);
+
+print_r($arr2);
+echo "<br>";
+echo "<br>";
+
+print_r($removidos2);
+
+
