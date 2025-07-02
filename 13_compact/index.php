e900b78965eeb24b6175bd463d34876ea97c10a2
Renato Cruz Souza <renato.souza.flooxmongagua@gmail.com>
2025-07-01 21:16:34 -0300

    feat: use compact with foreach to print array keys and values

--- 13_compact/index.php
+++ 13_compact/index.php
@@ -8,3 +8,21 @@ $portas = 4;
 $carro = compact("marca", "motor", "tetoSolar", "portas");
 
 print_r($carro);
+
+echo "<br>";
+echo "<br>";
+
+$raca = "vira-lata";
+$nome = "Zeca";
+$idade = 3;
+$cor = "caramelo";
+
+$zeca = compact("raca","nome", "idade", "cor");
+
+print_r($zeca);
+echo "<br>";
+echo "<br>";
+
+foreach($zeca as $caracteristicas => $value) {
+    echo " $caracteristicas: $value";
+}
